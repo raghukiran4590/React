6773183e03257b44086316f8069732d13361fb46
Raghu Kiran <raghu.kiran@elevancehealth.com>
2025-03-06 22:11:16 -0500

    Components

--- app.js
+++ app.js
@@ -6,26 +6,59 @@ import ReactDOM from "react-dom/client";
 //Emoji shortcut CTRL+CMD+SPACE
 
 //syntax using React Core
-const heading = React.createElement(
+/* const heading = React.createElement(
     "h1",
     { id: "heading" },
     "Namaste React 🚀"
-);
+); */
 
-console.log(heading);
+/* console.log(heading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
+root.render(heading); */
 
 // Using JSX
 // JSX code is transpiled before it reaches the JS engine - means it converts the ES6 compliant code to browser compliant standards
 // JSX => React.createElement => JavaScript Object => HTMLElement(render)
-const jsxHeading = <h1 id="jsxHeading">Namaste React using JSX 🚀</h1>;
+/* const jsxHeading = <h1 id="jsxHeading">Namaste React using JSX 🚀</h1>;
 const jsxHeading2 = (<h2 id="jsxHeading2">
     Heading2
-</h2>);
+</h2>); */
 
-console.log(jsxHeading);
+// console.log(jsxHeading);
 
-root.render(jsxHeading);
+//React Functional Component
+const Title = () => (
+    <div>
+        <h1>Title Heading in React</h1>
+        <p>Title Para</p>
+        <h2 id = "heading2">Title Heading 2</h2>
+        {"Hello World"}
+        <br></br>
+        {100+200}
+        {console.log("Logging")}
+    </div>
+    
+);
+
+const Func = () => (
+    <div>
+        <h1>Func Heading</h1>
+        {console.log("func testing")}
+    </div>
+);
+
+const HeadingComponent = () => (
+    <div id="container">
+    {<Title/>}
+    {<Func/>}
+    <h1 className="heading">Namaste React using Functional Component 🚀</h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
+
+
+// If the const is declared with () => {}, it will not render any html code. We can't JSX code if function is within the curly {} braces
+// If the const is declared with () => (), it will render it as JSX code. We can enclose the JS code within curly {} braces.
